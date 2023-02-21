c4f043079eb9321c5f2b85dcaea3873f8160d37f
Laryssa Alves De Carvalho <laryssa.carvalho@ambevtech.com.br>
2023-02-21 15:58:56 -0300

    Adjusts on responsiveness for mobile and tablet screens Added error messages on inputs

--- src/app/components/calculate-score-form/calculate-score-form.component.ts
+++ src/app/components/calculate-score-form/calculate-score-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {  FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { faDollarSign } from '@fortawesome/free-solid-svg-icons';
+import { ToastService } from 'src/app/services/toast.service';
 import { CalculateScoreRequest } from '../../models/calculate-score-request.model';
 import { FinancialScoreService } from '../../services/financial-score.service';
 
@@ -15,25 +16,36 @@ export class CalculateScoreFormComponent {
   faDollarSign = faDollarSign;
 
   scoreFormGroup = new FormGroup({
-    "annual-income": new FormControl<number | null>(null, [Validators.required, Validators.min(1)]),
-    "monthly-costs": new FormControl<number | null>(null, [Validators.required, Validators.min(1)]),
+    annualIncome: new FormControl<number | null>(null, [Validators.required, Validators.min(1)]),
+    monthlyCosts: new FormControl<number | null>(null, [Validators.required, Validators.min(1)]),
   });
 
-  constructor(private service: FinancialScoreService, private readonly router: Router) { }
+  constructor(private service: FinancialScoreService, private readonly router: Router) {   
+    
+  }
   
   onBtnClick(){
     if(this.scoreFormGroup.valid){
       var request = new CalculateScoreRequest ();
       
-      request.annualIncome = this.scoreFormGroup.controls['annual-income'].value!;
-      request.monthlyCosts = this.scoreFormGroup.controls['monthly-costs'].value!;      
+      request.annualIncome = this.scoreFormGroup.controls.annualIncome.value!;
+      request.monthlyCosts = this.scoreFormGroup.controls.monthlyCosts.value!;
       
       this.service.calculateScore(request).subscribe({      
         next: (r) => this.router.navigate(['score-result', { score: r.score }]),
-        error: (e) => console.log(e)
+        error: (e) => {
+          console.log(e) 
+        }
       });
       
       this.scoreFormGroup.reset();
+    } else if(this.scoreFormGroup.pristine && !this.scoreFormGroup.touched){
+      this.scoreFormGroup.controls.annualIncome.markAsTouched();
+      this.scoreFormGroup.controls.monthlyCosts.markAsTouched();
     }
   }
+
+  get annualIncome() { return this.scoreFormGroup.get('annualIncome'); }
+
+  get monthlyCosts() { return this.scoreFormGroup.get('monthlyCosts'); }
 }
